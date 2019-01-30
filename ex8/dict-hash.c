565bd73f4b3872b40ef9e60ac99bcb63e2357505
Wenchang Liu <wenchang.liu@student.manchester.ac.uk>
2019-01-30 13:56:04 +0000

    hash bug fixed

--- ex8/dict-hash.c
+++ ex8/dict-hash.c
@@ -28,6 +28,212 @@ struct table
 	float collisions;
 };
 
+Table_size hash_func (Key_Type key, int type);
+Table_size compress_func (Table_size index, Table_size table_size);
+Table_size collision_func(Table_size index, int type, Table_size index0);
+
+Table initialize_table (Table_size size) 
+{
+	Table head = (Table)malloc(sizeof(struct table));
+	head->cells = (cell*)malloc(size * sizeof(cell));
+	for(int i = 0; i < size; i++)
+	{
+		head->cells[i].element = NULL;
+		head->cells[i].state = empty;
+	}
+	head->table_size = size;
+	head->num_entries = 0;
+	head->collisions = 0;
+	return head;
+}
+
+Table insert (Key_Type new_key, Table head) 
+{
+	// select the hash and probing method
+	int hash_type = poly, probing_type = double_hash;
+	if(mode == 4)
+	{
+		hash_type = lin_hash;
+		probing_type = quadratic;
+	}
+	else if(mode == 3)
+	{
+		hash_type = poly;
+		probing_type = linear;
+	}
+	else if(mode == 2)
+	{
+		hash_type = poly;
+		probing_type = quadratic;
+	}
+	else if(mode == 1)
+	{
+		hash_type = poly;
+		probing_type = double_hash;
+	}
+	if(head->num_entries == head->table_size)
+	{
+		printf("Hash table is full.\n");
+		// exit or rehash
+		exit(0);
+	}
+	cell* p = head->cells;
+	Table_size index = hash_func(new_key, hash_type);
+	Table_size index0 = index;
+	index = compress_func(index, head->table_size);
+	// if(strcmp("fare", new_key)==0)
+	// {
+	// 	printf("%d\n", index);
+	// }
+	collision_func(index, clear, index0);
+	while(p[index].state == in_use)
+	{
+		// check duplicate
+		if(strcmp(p[index].element, new_key) == 0)
+		{
+			return head;
+		}
+		index = collision_func(index, probing_type, index0);
+	// 	if(strcmp("fare", new_key)==0)
+	// {
+	// 	printf("%d\n", index);
+	// }
+		head->collisions++;
+		if(index >= head->table_size)
+		{
+			index = compress_func(index, head->table_size);
+		}
+	}
+	p[index].element = (char*)malloc(WORD_SIZE);
+	strcpy(p[index].element, new_key);
+	p[index].state = in_use;
+	head->num_entries++;
+	if(probing_type == quadratic || probing_type == double_hash)
+	{
+		collision_func(index, clear, index0);
+	}
+	// check rehash
+	if(AUTO_REHASH && head->table_size / head->num_entries < 2)
+	{
+		// rehash when used / total > 0.5
+		Table_size new_size = head->table_size * 2;
+		Table new_head = initialize_table(new_size);
+		int i = 0, cnt = 0;
+		p = head->cells;
+		while(cnt < head->num_entries)
+		{
+			if(p[i].state == in_use)
+			{
+				new_head = insert(p[i].element, new_head);
+				cnt++;
+			}
+			i++;
+		}
+		tidy(head);
+		head = new_head;
+	}
+	return head;
+}
+
+Boolean find (Key_Type new_key, Table head) 
+{
+	int hash_type = poly, probing_type = double_hash;
+	if(mode == 4)
+	{
+		hash_type = lin_hash;
+		probing_type = quadratic;
+	}
+	else if(mode == 3)
+	{
+		hash_type = poly;
+		probing_type = linear;
+	}
+	else if(mode == 2)
+	{
+		hash_type = poly;
+		probing_type = quadratic;
+	}
+	else if(mode == 1)
+	{
+		hash_type = poly;
+		probing_type = double_hash;
+	}
+	cell* p = head->cells;
+	Table_size index = hash_func(new_key, hash_type);
+	Table_size index0 = index;
+	index = compress_func(index, head->table_size);
+	// if(strcmp("fare", new_key)==0)
+	// {
+	// 	printf("%d\n", index);
+	// }
+	Table_size start_point = index;
+	// if hash result is empty, then not found
+	if(p[index].state == empty)
+	{
+		return FALSE;
+	}
+	while(strcmp(p[index].element, new_key) != 0)
+	{
+		index = collision_func(index, probing_type, index0);
+		if(index >= head->table_size)
+		{
+			index = compress_func(index, head->table_size);
+		}
+	// 	if(strcmp("fare", new_key)==0)
+	// {
+	// 	printf("%d\n", index);
+	// }
+		// probing through all hash and still not found or reach empty
+		if( p[index].state == empty)
+		{
+			if(probing_type == quadratic || probing_type == double_hash)
+			{
+				collision_func(index, clear, index0);
+			}
+			return FALSE;
+		}
+	}
+	// found a match
+	if(probing_type == quadratic || probing_type == double_hash)
+	{
+		collision_func(index, clear, index0);
+	}
+	return TRUE;
+}
+
+void print_table (Table head) 
+{
+	cell* p = head->cells;
+	for(int i = 0; i < head->table_size; i++)
+	{
+		if(p[i].state == in_use)
+		{
+			printf("index: %d, key: %s\n", i, p[i].element);
+		}
+	}
+}
+
+void print_stats (Table head) 
+{
+	float avr_collision = head->collisions / head->num_entries;
+	printf("total collisions: %.0f\n", head->collisions);
+	printf("average collision per access: %.3f\n", avr_collision);
+}
+
+void tidy (Table head)
+{
+	cell* p = head->cells;
+	for(int i = 0; i < head->table_size; i++)
+	{
+		if(p[i].state == in_use)
+		{
+			free(p[i].element);
+		}
+	}
+	free(p);
+	free(head);
+}
+
 // hash function
 Table_size hash_func (Key_Type key, int type)
 {
@@ -94,189 +300,4 @@ Table_size collision_func(Table_size index, int type, Table_size index0)
 		index = index + index2;
 	}
 	return index;
-}
-
-Table initialize_table (Table_size size) 
-{
-	Table head = (Table)malloc(sizeof(struct table));
-	head->cells = (cell*)malloc(size * sizeof(cell));
-	for(int i = 0; i < size; i++)
-	{
-		head->cells[i].element = NULL;
-		head->cells[i].state = empty;
-	}
-	head->table_size = size;
-	head->num_entries = 0;
-	head->collisions = 0;
-	return head;
-}
-
-Table insert (Key_Type new_key, Table head) 
-{
-	// select the hash and probing method
-	int hash_type = poly, probing_type = double_hash;
-	if(mode == 4)
-	{
-		hash_type = lin_hash;
-		probing_type = quadratic;
-	}
-	else if(mode == 3)
-	{
-		hash_type = poly;
-		probing_type = linear;
-	}
-	else if(mode == 2)
-	{
-		hash_type = poly;
-		probing_type = quadratic;
-	}
-	else if(mode == 1)
-	{
-		hash_type = poly;
-		probing_type = double_hash;
-	}
-	if(head->num_entries == head->table_size)
-	{
-		printf("Hash table is full.\n");
-		// exit or rehash
-		exit(0);
-	}
-	cell* p = head->cells;
-	Table_size index = hash_func(new_key, hash_type);
-	Table_size index0 = index;
-	index = compress_func(index, head->table_size);
-	while(p[index].state == in_use)
-	{
-		// check duplicate
-		if(strcmp(p[index].element, new_key) == 0)
-		{
-			return head;
-		}
-		index = collision_func(index, probing_type, index0);
-		head->collisions++;
-		if(index >= head->table_size)
-		{
-			index = compress_func(index, head->table_size);
-		}
-	}
-	p[index].element = (char*)malloc(WORD_SIZE);
-	strcpy(p[index].element, new_key);
-	p[index].state = in_use;
-	head->num_entries++;
-	if(probing_type == quadratic || probing_type == double_hash)
-	{
-		collision_func(index, clear, index0);
-	}
-	// check rehash
-	if(AUTO_REHASH && head->table_size / head->num_entries < 2)
-	{
-		// rehash when used / total > 0.5
-		Table_size new_size = head->table_size * 2;
-		Table new_head = initialize_table(new_size);
-		int i = 0, cnt = 0;
-		p = head->cells;
-		while(cnt < head->num_entries)
-		{
-			if(p[i].state == in_use)
-			{
-				new_head = insert(p[i].element, new_head);
-				cnt++;
-			}
-			i++;
-		}
-		tidy(head);
-		head = new_head;
-	}
-	return head;
-}
-
-Boolean find (Key_Type new_key, Table head) 
-{
-	int hash_type = poly, probing_type = double_hash;
-	if(mode == 4)
-	{
-		hash_type = lin_hash;
-		probing_type = quadratic;
-	}
-	else if(mode == 3)
-	{
-		hash_type = poly;
-		probing_type = linear;
-	}
-	else if(mode == 2)
-	{
-		hash_type = poly;
-		probing_type = quadratic;
-	}
-	else if(mode == 1)
-	{
-		hash_type = poly;
-		probing_type = double_hash;
-	}
-	cell* p = head->cells;
-	Table_size index = hash_func(new_key, hash_type);
-	Table_size index0 = index;
-	index = compress_func(index, head->table_size);
-	Table_size start_point = index;
-	// if hash result is empty, then not found
-	if(p[index].state == empty)
-	{
-		return FALSE;
-	}
-	while(strcmp(p[index].element, new_key) != 0)
-	{
-		index = collision_func(index, probing_type, index0);
-		if(index >= head->table_size)
-		{
-			index = compress_func(index, head->table_size);
-		}
-		// probing through all hash and still not found or reach empty
-		if(index == start_point || p[index].state == empty)
-		{
-			if(probing_type == quadratic || probing_type == double_hash)
-			{
-				collision_func(index, clear, index0);
-			}
-			return FALSE;
-		}
-	}
-	// found a match
-	if(probing_type == quadratic || probing_type == double_hash)
-	{
-		collision_func(index, clear, index0);
-	}
-	return TRUE;
-}
-
-void print_table (Table head) 
-{
-	cell* p = head->cells;
-	for(int i = 0; i < head->table_size; i++)
-	{
-		if(p[i].state == in_use)
-		{
-			printf("index: %d, key: %s\n", i, p[i].element);
-		}
-	}
-}
-
-void print_stats (Table head) 
-{
-	float avr_collision = head->collisions / head->num_entries;
-	printf("total collisions: %.0f\n", head->collisions);
-	printf("average collision per access: %.3f\n", avr_collision);
-}
-
-void tidy (Table head)
-{
-	cell* p = head->cells;
-	for(int i = 0; i < head->table_size; i++)
-	{
-		if(p[i].state == in_use)
-		{
-			free(p[i].element);
-		}
-	}
-	free(p);
-	free(head);
 }
